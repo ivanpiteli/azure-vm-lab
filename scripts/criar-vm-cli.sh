385cf049ff8a02447765b22b324652f80bc0f1ea
ivanpiteli <ivan.piteli@gmail.com>
2025-08-18 22:00:42 -0300

    Update criar-vm-cli.sh

--- scripts/criar-vm-cli.sh
+++ scripts/criar-vm-cli.sh
@@ -5,7 +5,7 @@
 az login
 
 # Criar grupo de recursos
-az group create --name LabGrupo --location brazilsouth
+az group create --name LabGrupo-01 --location brazilsouth
 
 # Criar VM Ubuntu
-az vm create   --resource-group LabGrupo   --name LabVM   --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys
+az vm create   --resource-group LabGrupo-01  --name LabVM-01   --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys
